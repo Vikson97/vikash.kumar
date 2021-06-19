67746eb6ff04ff7e3263955d6f1b1cbe2b19252a
vikash.kumar <vikash.kumar@stackroute.in>
2021-06-19 11:01:40 +0530

    Updates

---
+++ precache-manifest.54a4f328c5deb04c7482af5751f2c887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbe0192c701d94716b88",
+    "url": "/vikash.kumar/static/css/main.ea7a9500.chunk.css"
+  },
+  {
+    "revision": "fbe0192c701d94716b88",
+    "url": "/vikash.kumar/static/js/main.fbe0192c.chunk.js"
+  },
+  {
+    "revision": "fce623415f7f07609965",
+    "url": "/vikash.kumar/static/js/runtime~main.fce62341.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/vikash.kumar/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "0fa433aa4b48f3630a419f7c759abff0",
+    "url": "/vikash.kumar/index.html"
+  }
+];
